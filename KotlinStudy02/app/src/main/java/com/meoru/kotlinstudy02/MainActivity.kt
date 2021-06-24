a21e87301a2eeae9c7655c1d4c0356fd0101a23b
sangilyoon-dev <sangilyoon.dev@gmail.com>
2021-06-24 16:16:46 +0900

    210624 first

--- KotlinStudy02/app/src/main/java/com/meoru/kotlinstudy02/MainActivity.kt
+++ KotlinStudy02/app/src/main/java/com/meoru/kotlinstudy02/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.meoru.kotlinstudy02
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import com.meoru.kotlinstudy02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    var first:Int? = 0
+    var second:Int? = 0
+    var third:Int? = 0
+    var fourth:Int? = 0
+    var fifth:Int? = 0
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var total:Int? =
-            editText.text.toString() as Int + editText2.text.toString() as Int + editText3.text.toString() as Int + editText4.text.toString() as Int + editText5.text.toString() as Int
-        button.setOnClickListener {
-            textView.setText("결과 : $total")
+        binding.button.setOnClickListener {
+
+            val firststr:String? = binding.editText.text.toString()
+            first = firststr?.toIntOrNull() ?: 0
+            val secondstr:String? = binding.editText2.text.toString()
+            second = secondstr?.toIntOrNull() ?: 0
+            val thirdstr:String? = binding.editText3.text.toString()
+            third = thirdstr?.toIntOrNull() ?: 0
+            val fourthstr:String? = binding.editText4.text.toString()
+            fourth = fourthstr?.toIntOrNull() ?: 0
+            val fifthstr:String? = binding.editText5.text.toString()
+            fifth = fifthstr?.toIntOrNull() ?: 0
+            val total = first!! + second!! + third!! + fourth!! + fifth!!
+            binding.textView.setText("결과 : ${total}")
         }
     }
 }
